d8765a21f9983de56e0568ef31a430a1550f61f5
thuong.ntd <thuong.ntd@sutrixsolutions.com>
2018-05-23 17:33:02 +0700

    add routes

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+// import React from 'react';
+// import { HomePage } from './pages/HomePage/HomePage';
+// import { SignIn } from './pages/SignIn/SignIn';
+
+// const routes = [
+//   {
+//     path: '/#/',
+//     exact: true,
+//     main: () => <Homepage />
+//   },
+//   {
+//     path: '/#/login',
+//     exact: true,
+//     main: () => <SignIn />
+//   }
+// ];
+// export default routes;
