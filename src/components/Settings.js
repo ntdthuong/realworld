cb81f4a56abb9af985a31ae961a602f198c38e52
aiduc93 <aiduc93@gmail.com>
2018-05-25 23:29:29 +0700

    revert code

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -4,67 +4,36 @@ import PropTypes from 'prop-types';
 import { profileAction } from '../actions';
 
 export class Settings extends Component {
-  // static defaultProps = {
-  //   user: {
-  //     image: '',
-  //     username: '2',
-  //     bio: '3',
-  //     email: '4',
-  //     password: ''
-  //   }
-  // };
+  static defaultProps = {
+    user: {
+      image: '',
+      username: '2',
+      bio: '3',
+      email: '4',
+      password: ''
+    }
+  };
 
-  constructor(props) {
-    super(props);
-    console.log('props in constructor', props);
-    // console.log('defaultProps in constructor', defaultProps);
-    const { image, username, bio, email, password } = props.user;
+  constructor(props, defaultProps) {
+    super(props, defaultProps);
+    const { image, username, bio, email, password} = props.user;
     this.state = {
       image,
       username,
       bio,
       email,
       password
-    };
-  }
-  // componentWillReceiveProps(newProps, prevProps) {
-  //   console.log('newProps',newProps);
-  //   console.log('prevProps',prevProps);
-  // }
-  static getDerivedStateFromProps(nextProps, prevState) {
-    console.log('newProps', nextProps);
-    console.log('prevProps', prevState);
-    if (nextProps.user !== prevState) {
-      return ({
-        image: nextProps.user.image || '',
-        username: nextProps.user.username || '',
-        bio: nextProps.user.bio || '',
-        email: nextProps.user.email || '',
-        password: nextProps.user.password || ''
-      });
-      return;
     }
-    return null;
   }
 
-  componentDidUpdate() {
-    // return this.setState({
-    //   image: nextProps.user.image,
-    //   username: nextProps.user.username,
-    //   bio: nextProps.user.bio,
-    //   email: nextProps.user.email,
-    //   password: nextProps.user.password
-    // });
-  }
   onChangeName = (e) => {
-    // const { value } = e.target;
-    this.setState({[e.target.name]: e.target.value})
+    const {value} = e.target;
+    this.setState({...this.state, username: value})
   }
 
   render() {
     const { user } = this.props;
-    const { username, bio, email, password, image } = this.state;
-    console.log('bio', bio);
+    console.log('username', this.state.username)
     return (
       <div className="settings-page">
         <div className="container page">
@@ -75,52 +44,48 @@ export class Settings extends Component {
 
               <form>
                 <fieldset>
-                  <fieldset className="form-group">
-                    <input
-                      className="form-control"
-                      type="text"
-                      placeholder="URL of profile picture"
-                      value={this.state.image}
-                      onChange={(value) => this.onChange(value)}
-                    />
-                  </fieldset>
-                  <fieldset className="form-group">
-                    <input
-                      className="form-control form-control-lg"
-                      type="text" placeholder="Your Name"
-                      value={this.state.username}
-                      onChange={(value) => this.onChange(value)}
-                    />
-                  </fieldset>
-                  <fieldset className="form-group">
-                    <textarea
-                      className="form-control form-control-lg"
-                      rows="8"
-                      placeholder="Short bio about you"
-                      value={this.state.bio}
-                      onChange={(value) => this.onChange(value)}
-                    ></textarea>
-                  </fieldset>
-                  <fieldset className="form-group">
-                    <input
-                      className="form-control form-control-lg"
-                      type="text"
-                      placeholder="Email"
-                      value={this.state.email}
-                      onChange={(value) => this.onChange(value)}
-                    />
-                  </fieldset>
-                  <fieldset className="form-group">
-                    <input
-                      className="form-control form-control-lg"
-                      type="password"
-                      placeholder="Password"
-                      value={this.state.password}
-                      onChange={(value) => this.onChange(value)}
-                    />
-                  </fieldset>
-                  <button className="btn btn-lg btn-primary pull-xs-right">
-                    Update Settings
+                    <fieldset className="form-group">
+                      <input
+                        className="form-control"
+                        type="text"
+                        placeholder="URL of profile picture"
+                        defaultValue={this.state.image}
+                      />
+                    </fieldset>
+                    <fieldset className="form-group">
+                      <input
+                        className="form-control form-control-lg"
+                        type="text" placeholder="Your Name"
+                        defaultValue={this.state.username}
+                        onChange={this.onChangeName}
+                      />
+                    </fieldset>
+                    <fieldset className="form-group">
+                      <textarea
+                        className="form-control form-control-lg"
+                        rows="8"
+                        placeholder="Short bio about you"
+                        defaultValue={this.state.bio}
+                      ></textarea>
+                    </fieldset>
+                    <fieldset className="form-group">
+                      <input
+                        className="form-control form-control-lg"
+                        type="text"
+                        placeholder="Email"
+                        defaultValue={this.state.email}
+                      />
+                    </fieldset>
+                    <fieldset className="form-group">
+                      <input
+                        className="form-control form-control-lg"
+                        type="password"
+                        placeholder="Password"
+                        defaultValue={this.state.password}
+                      />
+                    </fieldset>
+                    <button className="btn btn-lg btn-primary pull-xs-right">
+                      Update Settings
                     </button>
                 </fieldset>
               </form>
