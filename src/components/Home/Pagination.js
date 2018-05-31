d43384acaf16b773e7126dd341cbbc06cf62e38a
Duc Vuong <duc.vuong@sutrixsolutions.com>
2018-05-31 13:45:07 +0700

    fixed bug paging

--- src/components/Home/Pagination.js
+++ src/components/Home/Pagination.js
@@ -5,11 +5,26 @@ export class Pagination extends Component {
     super(props);
     this.state = {
       page: 1,
+      currentPage: 1,
+      articleTag: '',
     };
   }
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    if ((prevState.currentPage !== nextProps.pageNow) || ((prevState.articleTag !== '') && (prevState.articleTag !== nextProps.articleTag))) {
+      const currentPage = nextProps.pageNow;
+      return {
+        page: 1,
+        currentPage,
+        articleTag: nextProps.articleTag
+      };
+    }
+    return null;
+  }
+
   handleClick = (i, e) => {
     e.preventDefault();
-    this.setState({page: i});
+    this.setState({ page: i });
     const {
       pageNow,
       onFetchPaging,
@@ -17,18 +32,20 @@ export class Pagination extends Component {
       onFetchArticleByTag,
       articleTag
     } = this.props;
-    if(pageNow === 'feed') onFetchArticleByUser(i);
-    if(pageNow === 'global') onFetchPaging(i);
-    if(pageNow === 'tag') onFetchArticleByTag(articleTag, i);
+    if (pageNow === 'feed') onFetchArticleByUser(i);
+    if (pageNow === 'global') onFetchPaging(i);
+    if (pageNow === 'tag') onFetchArticleByTag(articleTag, i);
   }
+
   genPaging = () => {
     const { articlesCount } = this.props;
     const { page } = this.state;
     let arrPage = [];
-    for(let i=1; i<= Math.ceil(articlesCount/10); i++) {
+    for (let i = 1; i <= Math.ceil(articlesCount / 10); i++) {
       const customClass = page === i ? 'page-item active' : 'page-item';
       arrPage.push(
         <li
+          key={i}
           className={customClass}
           onClick={(e) => this.handleClick(i, e)}
         >
